a4ccd973ed89fb91f008c3d3fa486d2e2c1aa1bb
Nicolas <jacquemin.nicolas59@gmail.com>
2023-08-03 19:55:01 +0200

    [+] Init Vue Project + Generate favicon pack

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import './assets/main.css'
+import router from './router/router'
+
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App)
+.use(router)
+.use(createPinia())
+.mount('#app')
